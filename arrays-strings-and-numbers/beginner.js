a3a57bd12ca730e700caee72f1b637bf134f4a08
GoodnessObi <goodnessobi@gmail.com>
2022-03-25 23:41:22 +0100

    exercize day 1

---
+++ arrays-strings-and-numbers/beginner.js
@@ -0,0 +1,65 @@
+/* 
+1. Given a binary array nums, return the maximum number of consecutive 1's in the array.
+Constraints:
+1 <= nums.length <= 105
+nums[i] is either 0 or 1.
+*/
+var findMaxConsecutiveOnes = function(nums) {
+  /* keep track of the count when it is 1 */
+  count = 0;
+  /* when it hits 0, compare the present count with the last count and keep the higher value */
+  maxCount = 0;
+  
+  for ( let i = 0; i < nums.length; i++) {
+      if (nums[i] === 1) {
+          count++
+      } else {
+          maxCount = Math.max(maxCount, count)
+          count = 0
+      }
+  }
+  return Math.max(maxCount, count)
+};
+
+/*
+2. Given an array nums of integers, return how many of them contain an even number of digits.
+Constraints: 
+1 <= nums.length <= 500
+1 <= nums[i] <= 105
+*/
+var findNumbers = function(nums) {
+  /* loop through each number and divide by 10 till the result is less than 1 = 0.9
+  the number of times looped through is the count of the number
+  if the number of times looped through is divisible by 2, it is even
+  nums = [555,901,482,1771]
+  nums = [12,345,2,6,7896]
+  */
+  let evenCount = 0;
+  
+  for ( let i = 0; i < nums.length; i++ ) {
+      let value = nums[i];
+      let digitCount = 0
+      while (value >= 1) {
+          value = value/10
+          digitCount++
+      }
+      if ( digitCount % 2 == 0 ) evenCount++
+  }
+  return evenCount;
+};
+
+var findNumbers = function(nums) {
+  /* loop through each number and divide by 10 till the result is less than 1 = 0.9
+  the number of times looped through is the count of the number
+  if the number of times looped through is divisible by 2, it is even
+  nums = [555,901,482,1771]
+  nums = [12,345,2,6,7896]
+  */
+  let evenCount = 0;
+  
+  for ( let i = 0; i < nums.length; i++ ) {
+      const digitCount = nums[i].toString().length;
+      if ( digitCount % 2 == 0 ) evenCount++
+  }
+  return evenCount;
+};
