5a17a39a78da14dc1ecf16633a7ff6ef8381b539
GoodnessObi <goodnessobi@gmail.com>
2022-03-29 00:03:22 +0100

    exercize day 3a

--- arrays-strings-and-numbers/beginner.js
+++ arrays-strings-and-numbers/beginner.js
@@ -65,8 +65,7 @@ var findNumbers = function(nums) {
 };
 
 /*
-Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
-Constraints:
+3. Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
 Constraints:
 
 1 <= nums.length <= 104
@@ -84,4 +83,26 @@ var sortedSquares = function(nums) {
   return sortedArr.sort(function(a, b) {
     return a - b;
   })
+};
+
+/*
+3. Given a fixed-length integer array arr, duplicate each occurrence of zero, shifting the remaining elements to the right.
+
+Note that elements beyond the length of the original array are not written. Do the above modifications to the input array in place and do not return anything.
+
+Example:
+1. Input: arr = [1,0,2,3,0,4,5,0]
+Output: [1,0,0,2,3,0,0,4]
+Explanation: After calling your function, the input array is modified to: [1,0,0,2,3,0,0,4]
+
+2. Input: arr = [1,2,3]
+Output: [1,2,3]
+Explanation: After calling your function, the input array is modified to: [1,2,3]
+ 
+Constraints:
+1 <= arr.length <= 104
+0 <= arr[i] <= 9
+*/
+var duplicateZeros = function(arr) {
+
 };
