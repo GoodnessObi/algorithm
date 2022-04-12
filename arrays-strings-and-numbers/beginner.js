44a1e22e09fc1051323be0a0152ac3e8c72b14d0
GoodnessObi <goodnessobi@gmail.com>
2022-04-12 23:38:51 +0100

    exercise day 4b and 5

--- arrays-strings-and-numbers/beginner.js
+++ arrays-strings-and-numbers/beginner.js
@@ -188,12 +188,98 @@ var removeElement = function(nums, val) {
 }
 
 var removeElement = function(nums, val) {
-  let i = 0;
+  let n = 0;
   for (let j = 0; j < nums.length; j++) {
       if (nums[j] !== val) {
-          nums[i] = nums[j]
+          nums[n] = nums[j]
           i++;
       }
   }
-  return i;
+  return n;
 }
+
+/*
+5. Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+Example:
+1. Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+2. Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+ 
+Constraints:
+1 <= nums.length <= 3 * 104
+-100 <= nums[i] <= 100
+nums is sorted in non-decreasing order.
+
+*/
+// Input: nums = [0,0,1,1,1,2,2,3,3,4]
+// Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+var removeDuplicates = function(nums) {
+  // keep the iterating index (i) of unique characters.
+  // We start at nums[1] cause the first number in a non-decreasing/sorted array will be unique
+  // the iterating index just determines where teh next unique character goes
+  // Every character after the unique characters do not matter in an in-place operation
+  // return the iterating character as it gives the number of unique characters left
+  
+  let i = 1;
+  
+  for (let j = 1; j < nums.length; j++) {
+      if (nums[j] !== nums[i - 1]) {
+          nums[i] = nums[j]
+          i++
+      }
+  }
+  return i;
+};
+
+/*
+6. Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears at most twice. The relative order of the elements should be kept the same.
+
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+Example:
+1. Input: nums = [1,1,1,2,2,3]
+Output: 5, nums = [1,1,2,2,3,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+2. Input: nums = [0,0,1,1,1,1,2,3,3]
+Output: 7, nums = [0,0,1,1,2,3,3,_,_]
+Explanation: Your function should return k = 7, with the first seven elements of nums being 0, 0, 1, 1, 2, 3 and 3 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+ 
+Constraints:
+1 <= nums.length <= 3 * 104
+-104 <= nums[i] <= 104
+nums is sorted in non-decreasing order.
+
+*/
+// Input: nums = [1,1,1,2,2,3]
+// Output: 5, nums = [1,1,2,2,3,_]
+var removeDuplicates = function(nums) {
+  let i = 1;
+  
+  for (let j = 1; j < nums.length; j++) {
+      if (((nums[j] === nums[i - 1]) && (nums[j] !== nums[i - 2])) || (nums[j] !== nums[i - 1])) {
+          nums[i] = nums[j]
+          i++
+      }
+  }
+  return i;
+};
