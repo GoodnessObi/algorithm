520718fcb204b5f5508787d32ceee741fa61514e
GoodnessObi <goodnessobi@gmail.com>
2022-04-13 22:00:07 +0100

    exercise day 6b

--- arrays-strings-and-numbers/beginner.js
+++ arrays-strings-and-numbers/beginner.js
@@ -273,13 +273,48 @@ nums is sorted in non-decreasing order.
 // Input: nums = [1,1,1,2,2,3]
 // Output: 5, nums = [1,1,2,2,3,_]
 var removeDuplicates = function(nums) {
+  //initialize the counter
   let i = 1;
   
+  //Start from the second element and process elements one by one
   for (let j = 1; j < nums.length; j++) {
+    //left condition: if current element is the second duplicate;
+    //right condition: if the current element is the first of its kind
+    //really these are the two conditions needed
       if (((nums[j] === nums[i - 1]) && (nums[j] !== nums[i - 2])) || (nums[j] !== nums[i - 1])) {
-          nums[i] = nums[j]
-          i++
+          //overwrite the element at index/counter i
+
+          // nums[i] = nums[j]
+          // i++
+          nums[i++] = nums[j]
       }
   }
   return i;
+};
+
+//alternate
+var removeDuplicates = function(nums) {
+  // Initialize the counter and the second pointer.
+  let j = 1;
+  let count = 1
+   
+  // Start from the second element of the array and process
+  // elements one by one.
+  for (let i = 1; i < nums.length; i++) {
+    // If the current element is a duplicate, increment the count.
+    if (nums[i] === nums[i - 1]) {
+      count++
+    } else {
+      // Reset the count since we encountered a different element
+      // than the previous one.
+      count = 1
+    }
+    
+    // For a count <= 2, we copy the element over thus
+    // overwriting the element at index "j" in the array
+    if (count <= 2) {
+      nums[j++] = nums[i]
+    }
+  }
+  return j;
 };
